3f621d05af8d95cc7033876d3f72f4ea6acef8a4
MelisaRivas <rivasmelisa2024@gmail.com>
2023-06-03 17:53:12 -0600

    creando el menu del convertidor, copiar para intentar una segunda version

--- main.js
+++ main.js
@@ -1,41 +1,59 @@
-/* Se hizo un simulador de convertidor de masas y volumen, en donde el usuario puede escoger entre convertir de libras (lb) a gramos, litros (l) a milimetros (ml) y más */
-function agregarLibro(){
-    let autorIngresado = prompt("Ingrese el nombre del autor")
-    let tituloIngresado = prompt("Ingrese el nombre del titulo")
-    let precioIngresado = parseInt(prompt("Ingrese el nombre del precio"))
-    return mostrarDatosLibro(autorIngresado, tituloIngresado, precioIngresado)
+/* Se hizo un simulador de convertidor de masas y volumen, en donde el usuario puede escoger entre convertir de libras (lb) a gramos, litros (l) a mililitros (ml) y más */
 
+// Empezamos declarando variables para el convertidor
+//Kg a Lb
+function kgAlb(){
+   let kg = parseInt(prompt("Ingrese la cantidad de kilogramos (kg) que desea convertir a libras (lb): "))
+   let conversionAlb = kg * 2.20462
+   alert(`${kg}kg a libras son ${conversionAlb}lb`) 
 }
-// function que muestra los datos del libro
-function mostrarDatosLibro(autor, titulo, precio){
-    console.log(`El libro fue escrito por ${autor} su titulo es ${titulo} y su precio es ${precio}`)
+// Lb a g
+function lbAg(){
+   let lb = parseInt(prompt("Ingrese la cantidad de libras (l) que desea convertir a gramos (g): "))
+   let conversionAg = lb * 453.592
+   alert(`${lb}lb a gramos son ${conversionAg}g`) 
 }
-//MENU do while y switch 
+//Gal a l
+function galAl(){
+   let gal = parseInt(prompt("Ingrese la cantidad de galones (gal) que desea convertir a litros (l): "))
+   let conversionAl = gal * 3.78541
+   alert(`${gal}gal a litros son ${conversionAl}l`) 
+}
+// L a ml
+function lAml(){
+   let l = parseInt(prompt("Ingrese la cantidad de litros (l) que desea convertir a mililitro (ml): "))
+   let conversionAml = l * 1000
+   alert(`${l}l a mililitro son ${conversionAml}ml`) 
+}
+
+//MENÚ con ciclo do while y condicional switch 
 let salirMenu = false
-
 do{
-let opcionIngresada = parseInt(prompt(`Ingrese la opción deseada
-   1 - Agregar libro
-   2 - Borrar libro
-   3 - Consultar catálogo
+let opciones = parseInt(prompt(`¿Qué desea convertir?
+   1 - Convertir Kilogramos (kg) a libras (lb)
+   2 - Convertir libras (lb) a gramos (g)
+   3 - Convertir galones (gal) a litros (l)
+   4 - Convertir litros (l) a mililitro (ml)
    0 - Salir del menu`))
-   switch(opcionIngresada){
+   switch(opciones){
       case 1:
-         agregarLibro()
-         
+         kgAlb()
       break
       case 2:
-         console.log("Borrar libro")
+         lbAg()
       break
       case 3:
-         mostrarDatosLibro()
-      break         
+         galAl()
+      break    
+      case 4:
+         lAml()
+      break     
       case 0:
-         console.log(`Gracias por utilizar nuestra app. Saludos!`)
+         alert(`Gracias por utilizar nuestro convertidor. ¡Vuelve pronto!`)
          salirMenu = true
       break   
       default:
-         console.log("Opción no válida, ingrese alguna presente en el menu")
+         alert("Oh! oh!, no tenemos esa opción :(, intenta de nuevo.")
       break
    }
 }while(!salirMenu)
